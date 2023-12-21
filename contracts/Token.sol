072df4160abc3283590de0ec3e61c1f1af5a4803
jtaddrocket <nguyentiendat3_t66@hus.edu.vn>
2023-12-21 15:21:11 +0700

    update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity <=0.8.19;
+pragma solidity ^0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 contract Floppy is
     ERC20("Floppy", "FLP"),
     ERC20Burnable,
