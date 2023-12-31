7f3e782057b49e7045ca7d65453afa77d8714059
ahng03 <zukitata03@18l427.onmicrosoft.com>
2023-12-31 18:07:48 +0700

    Tạo ERC721 token, chỉnh sửa config của hardhat.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      { version: "0.8.19" }, // Compiler for Token.sol
+      { version: "0.8.20" }  // Compiler for DHB.sol and other files (if applicable)
+    ],
+    overrides: {
+      "contracts/Token.sol": {
+        version: "0.8.19",
+        settings: {}
+      }
+    }
+  },
   networks: {
     bsctest: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
